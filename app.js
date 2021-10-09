8fccf38d57b741a6a4fd1d2f77439125ac7aa67f
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-09 21:59:52 +0900

    created School parent class

---
+++ app.js
@@ -0,0 +1,35 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(num) {
+    if(typeof num !== "number") {
+      console.log('Invalid input: numberOfStudents must be set to a Number') 
+      } else {
+          this._numberOfStudents = num;
+      }
+  }
+
+  quickFacts() {
+    return `${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level`
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    return substituteTeachers[Math.floor(Math.random()*substituteTeachers.length)];
+  }
+}
