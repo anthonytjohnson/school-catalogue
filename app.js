b1f9fa6ea5324f7add24001426ab6bf3ad7a06a8
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-09 22:21:13 +0900

    tested classes, working, added todos

--- app.js
+++ app.js
@@ -55,3 +55,19 @@ class HighSchool extends School {
     console.log(this._sportsTeams);
   }
 }
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+console.log(lorraineHansbury.quickFacts());
+
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+alSmith.sportsTeams;
+
+
+// Create a middle school class
+// Add more properties to each class (averageTestScores, schoolOverview, etc.)
+// Create a class called SchoolCatalog that holds a collection of schools. Create an instance of SchoolCatalog for primary, middle, and high schools.
+
